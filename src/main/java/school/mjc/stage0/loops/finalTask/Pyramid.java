d15e69329cdb8c6a06ae4a5c939dad203d43254d
jargr <jaroslav.grablevski@gmail.com>
2023-02-26 18:55:54 +0200

    Final loops

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,6 +2,22 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int j = 0; j < cathetusLength - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j > 0; j--) {
+                System.out.print(j);
+            }
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j);
+            }
+
+
+            System.out.println();
+        }
+
+
 
     }
 
